411aead6e7c7ba88c2a0effea36eeffadea6c152
itskenshin <zomaliense01@gmail.com>
2022-04-03 15:41:10 -0400

    new stuff

--- BucketDonation/Form3.Designer.cs
+++ BucketDonation/Form3.Designer.cs
@@ -30,25 +30,137 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainScreen));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label_donadoresrecientes = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.tel_label = new System.Windows.Forms.Label();
+            this.nombre_label = new System.Windows.Forms.Label();
+            this.tipo_donacion_label = new System.Windows.Forms.Label();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(383, 30);
+            this.pictureBox1.Location = new System.Drawing.Point(421, 27);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(389, 243);
+            this.pictureBox1.Size = new System.Drawing.Size(334, 216);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(169, 339);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(28, 27);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label_donadoresrecientes
+            // 
+            this.label_donadoresrecientes.AutoSize = true;
+            this.label_donadoresrecientes.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label_donadoresrecientes.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_donadoresrecientes.Location = new System.Drawing.Point(131, 313);
+            this.label_donadoresrecientes.Name = "label_donadoresrecientes";
+            this.label_donadoresrecientes.Size = new System.Drawing.Size(123, 13);
+            this.label_donadoresrecientes.TabIndex = 2;
+            this.label_donadoresrecientes.Text = "Donadores Reciente";
+            this.label_donadoresrecientes.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox5);
+            this.panel1.Controls.Add(this.pictureBox4);
+            this.panel1.Controls.Add(this.tipo_donacion_label);
+            this.panel1.Controls.Add(this.nombre_label);
+            this.panel1.Controls.Add(this.tel_label);
+            this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.Location = new System.Drawing.Point(46, 386);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(305, 304);
+            this.panel1.TabIndex = 3;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(305, 304);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 0;
+            this.pictureBox3.TabStop = false;
+            // 
+            // tel_label
+            // 
+            this.tel_label.AutoSize = true;
+            this.tel_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tel_label.Location = new System.Drawing.Point(106, 47);
+            this.tel_label.Name = "tel_label";
+            this.tel_label.Size = new System.Drawing.Size(57, 13);
+            this.tel_label.TabIndex = 1;
+            this.tel_label.Text = "Teléfono";
+            // 
+            // nombre_label
+            // 
+            this.nombre_label.AutoSize = true;
+            this.nombre_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nombre_label.Location = new System.Drawing.Point(13, 47);
+            this.nombre_label.Name = "nombre_label";
+            this.nombre_label.Size = new System.Drawing.Size(50, 13);
+            this.nombre_label.TabIndex = 2;
+            this.nombre_label.Text = "Nombre";
+            // 
+            // tipo_donacion_label
+            // 
+            this.tipo_donacion_label.AutoSize = true;
+            this.tipo_donacion_label.BackColor = System.Drawing.SystemColors.Window;
+            this.tipo_donacion_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tipo_donacion_label.Location = new System.Drawing.Point(191, 38);
+            this.tipo_donacion_label.Name = "tipo_donacion_label";
+            this.tipo_donacion_label.Size = new System.Drawing.Size(108, 13);
+            this.tipo_donacion_label.TabIndex = 3;
+            this.tipo_donacion_label.Text = "Tipo de Donación";
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(13, 75);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(172, 200);
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(191, 56);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(100, 210);
+            this.pictureBox5.TabIndex = 5;
+            this.pictureBox5.TabStop = false;
             // 
             // MainScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Window;
-            this.ClientSize = new System.Drawing.Size(1173, 702);
+            this.ClientSize = new System.Drawing.Size(1173, 728);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label_donadoresrecientes);
+            this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
@@ -57,12 +169,28 @@
             this.Text = "BucketDonation";
             this.Load += new System.EventHandler(this.Form3_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label_donadoresrecientes;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label tel_label;
+        private System.Windows.Forms.Label nombre_label;
+        private System.Windows.Forms.Label tipo_donacion_label;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox4;
     }
 }
