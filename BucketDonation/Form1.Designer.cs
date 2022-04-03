3b5a8492e85402707095e88c18dc490fc9595a3a
itskenshin <zomaliense01@gmail.com>
2022-04-03 12:30:35 -0400

    help screen lista solo falta agregar logica para cambiar a la main screen

--- BucketDonation/Form1.Designer.cs
+++ BucketDonation/Form1.Designer.cs
@@ -28,22 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HelpScreen));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.boton_siguiente = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boton_siguiente)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(411, 487);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // boton_siguiente
+            // 
+            this.boton_siguiente.Image = ((System.Drawing.Image)(resources.GetObject("boton_siguiente.Image")));
+            this.boton_siguiente.Location = new System.Drawing.Point(160, 495);
+            this.boton_siguiente.Name = "boton_siguiente";
+            this.boton_siguiente.Size = new System.Drawing.Size(100, 50);
+            this.boton_siguiente.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.boton_siguiente.TabIndex = 1;
+            this.boton_siguiente.TabStop = false;
+            this.boton_siguiente.Click += new System.EventHandler(this.pictureBox2_Click);
             // 
             // HelpScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(383, 535);
+            this.ClientSize = new System.Drawing.Size(435, 557);
             this.ControlBox = false;
+            this.Controls.Add(this.boton_siguiente);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "HelpScreen";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Load += new System.EventHandler(this.HelpScreen_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boton_siguiente)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox boton_siguiente;
     }
 }
 
--- BucketDonation/Form1.cs
+++ BucketDonation/Form1.cs
@@ -36,5 +36,10 @@ namespace BucketDonation
         {
 
         }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            //
+        }
     }
 }
